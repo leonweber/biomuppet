43f20287b899bb55714bf2d282dd1dd07f1fefa5
Natasha Seelam <nseelam1@gmail.com>
2022-05-08 20:19:56 -0400

    adds blurb dataset fetch

---
+++ BLURB/README.md
@@ -0,0 +1,3 @@
+# Quantifying overlap of train/test BLURB datasets
+
+First, run the script `fetch_blurb.py`.
---
+++ BLURB/fetch_blurb.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+mkdir -p blurb_data
+cd blurb_data
+
+wget https://microsoft.github.io/BLURB/sample_code/data_generation.tar.gz
+tar -xf data_generation.tar.gz
