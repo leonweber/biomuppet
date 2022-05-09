1c57dfc7b16a0528aa78c58cb31af00db87650a7
Leon Weber <leonweber@posteo.de>
2022-05-09 13:28:04 +0200

    Add classification metrics and eval on dev to train_bunsen.py

--- biomuppet/train_bunsen.py
+++ biomuppet/train_bunsen.py
@@ -33,7 +33,7 @@ from biomuppet.relation_extraction import get_all_re_datasets
 
 pl.seed_everything(42)
 
-NUM_GPUS = 4
+NUM_GPUS = 1
 GRADIENT_ACCUMULATION = 1
 BATCH_SIZE = 32
 MAX_STEPS = 140000 * GRADIENT_ACCUMULATION
@@ -81,15 +81,44 @@ def multilabel_classification_loss(logits, labels, meta):
     cls_logit = logits[0]
     label_tensor = torch.zeros(len(meta.label_to_id))
     for label in labels:
-        label_tensor[meta.label_to_id[label]] = 1
+        label_tensor[meta.label_to_id.get(label, 0)] = 1
     loss = nn.BCEWithLogitsLoss()(cls_logit.unsqueeze(0), label_tensor.to(logits.device).unsqueeze(0))
     loss /= np.log(len(meta.label_to_id))
 
     return loss
 
 
+def classification_score(logits, labels, meta, f1):
+    assert logits.ndim == 2
+    cls_logit = logits[0]
+    label_tensor = torch.zeros(len(meta.label_to_id))
+    if labels:
+        label = labels[0]
+    else:
+        label = "None"
+
+    label_tensor[meta.label_to_id[label]] = 1
+
+    f1(cls_logit.cpu().unsqueeze(0)[:, 1:],
+       label_tensor.int().unsqueeze(0)[:, 1:]
+       )
+
+
+def multilabel_classification_score(logits, labels, meta, f1):
+    assert logits.ndim == 2
+    cls_logit = logits[0]
+    label_tensor = torch.zeros(len(meta.label_to_id))
+    for label in labels:
+        label_tensor[meta.label_to_id.get(label, 0)] = 1
+    f1(cls_logit.cpu().unsqueeze(0), label_tensor.int().unsqueeze(0))
+
+
 TASK_TYPE_TO_LOSS = {"clf": classification_loss,
                      "multilabel_clf": multilabel_classification_loss}
+
+TASK_TYPE_TO_SCORE = {"clf": classification_score,
+                     "multilabel_clf": multilabel_classification_score}
+
 
 @dataclasses.dataclass
 class ToableList:
@@ -118,7 +147,7 @@ class DatasetMetaInformation:
 @dataclass
 class MultitaskClassifierOutput(transformers.file_utils.ModelOutput):
     loss: Optional[torch.FloatTensor] = None
-    dataset_to_logits: Dict[str, torch.FloatTensor] = None
+    logits: List[torch.FloatTensor] = None
     hidden_states: Optional[Tuple[torch.FloatTensor]] = None
     attentions: Optional[Tuple[torch.FloatTensor]] = None
 
@@ -153,20 +182,16 @@ class BioMuppet(pl.LightningModule):
         self.dropout = nn.Dropout(dropout)
 
         self.dataset_to_out_layer = nn.ModuleDict()
-        self.dataset_to_train_f1 = {}
-        self.dataset_to_dev_f1 = {}
+        self.dataset_to_train_f1 = nn.ModuleDict()
+        self.dataset_to_dev_f1 = nn.ModuleDict()
 
         for dataset, meta in dataset_to_meta.items():
             out_layer = self.dataset_to_out_layer[dataset] = nn.Linear(
                 self.transformer.config.hidden_size, len(meta.label_to_id)
             )
             self.dataset_to_out_layer[dataset] = out_layer
-            self.dataset_to_train_f1[dataset] = torchmetrics.F1Score(
-                num_classes=len(meta.label_to_id)-1
-            )
-            self.dataset_to_dev_f1[dataset] = torchmetrics.F1Score(
-                num_classes=len(meta.label_to_id)-1
-            )
+            self.dataset_to_train_f1[dataset] = torchmetrics.F1Score(threshold=0.0)
+            self.dataset_to_dev_f1[dataset] = torchmetrics.F1Score(threshold=0.0)
 
         self.lr = lr
         self.num_training_steps = None
@@ -188,6 +213,7 @@ class BioMuppet(pl.LightningModule):
         return batch
 
     def forward(self, features):
+        all_logits = []
         if "token_type_ids" in features:
             output = self.transformer(
                 input_ids=features["input_ids"],
@@ -204,26 +230,48 @@ class BioMuppet(pl.LightningModule):
         batch_loss = 0
 
         for seq_emb, labels, meta in zip(batch_seq_emb, features["labels"], features["meta"]):
+
             logits = self.dataset_to_out_layer[meta.name](seq_emb)
+            all_logits.append(logits)
             batch_loss += TASK_TYPE_TO_LOSS[meta.type](logits, labels=labels, meta=meta)
 
         return MultitaskClassifierOutput(
             loss=batch_loss,
-            dataset_to_logits={},
+            logits=all_logits,
         )
 
     def training_step(self, batch, batch_idx):
         output = self.forward(batch)
 
-        # if self.lr_schedulers():
-        #     self.lr_schedulers().step()
-
+        for logits, labels, meta in zip(output["logits"], batch['labels'], batch["meta"]):
+            f1 = self.dataset_to_train_f1[meta.name]
+            TASK_TYPE_TO_SCORE[meta.type](logits, labels, meta, f1)
         self.log("train/loss", output.loss, prog_bar=True, on_step=True, on_epoch=True)
         return output.loss
 
-    # def training_epoch_end(self, outputs) -> None:
-    #     for dataset, train_f1 in self.dataset_to_train_f1.items():
-    #         self.log(f"{dataset}/train/f1_epoch", train_f1, prog_bar=False)
+    def validation_step(self, batch, batch_idx):
+        output = self.forward(batch)
+        for logits, labels, meta in zip(output["logits"], batch['labels'], batch["meta"]):
+            f1 = self.dataset_to_dev_f1[meta.name]
+            TASK_TYPE_TO_SCORE[meta.type](logits, labels, meta, f1)
+
+    def training_epoch_end(self, outputs) -> None:
+        f1_sum = 0
+        for dataset, train_f1 in self.dataset_to_train_f1.items():
+            if train_f1.tp + train_f1.fp + train_f1.fn > 0:
+                self.log(f"{dataset}/train/f1_epoch", train_f1, prog_bar=False)
+                f1_sum += train_f1
+        self.log(f"total/train/f1_epoch", f1_sum.cuda().compute(), prog_bar=True)
+
+    def validation_epoch_end(self, outputs) -> None:
+        f1_sum = 0
+        for dataset, dev_f1 in self.dataset_to_dev_f1.items():
+            if dev_f1.tp + dev_f1.fp + dev_f1.fn > 0:
+                self.log(f"{dataset}/dev/f1_epoch", dev_f1, prog_bar=False)
+                f1_sum += dev_f1
+        self.log(f"total/dev/f1_epoch", f1_sum.cuda().compute(), prog_bar=True)
+
+
 
 
     def configure_optimizers(self):
@@ -270,6 +318,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--output_dir", type=Path, required=True)
     parser.add_argument("--overwrite_output_dir", action="store_true")
+    parser.add_argument("--eval_only", action="store_true")
+    parser.add_argument("--load_checkpoint")
     args = parser.parse_args()
 
     if args.output_dir.exists() and not args.overwrite_output_dir:
@@ -288,39 +338,75 @@ if __name__ == '__main__':
 
     dataset_to_meta = {i.meta.name: i.meta for i in train_datasets}
 
-    model = BioMuppet(
-        transformer="michiyasunaga/BioLinkBERT-base",
-        lr=3e-5,
-        dataset_to_meta=dataset_to_meta,
-        use_lr_scheduler=True
-    )
+    if args.load_checkpoint:
+        model = BioMuppet.load_from_checkpoint(
+            checkpoint_path=args.load_checkpoint,
+            strict=False,
+            transformer="michiyasunaga/BioLinkBERT-base",
+            lr=3e-5,
+            dataset_to_meta=dataset_to_meta,
+            use_lr_scheduler=True
+        )
+    else:
+        model = BioMuppet(
+            transformer="michiyasunaga/BioLinkBERT-base",
+            lr=3e-5,
+            dataset_to_meta=dataset_to_meta,
+            use_lr_scheduler=True
+        )
+
 
     callbacks = []
     callbacks.append(ModelCheckpoint(dirpath=args.output_dir, every_n_train_steps=5000,
                                      save_top_k=-1))
     callbacks.append(LearningRateMonitor(logging_interval="step"))
-    mixed_train_instances = []
-    for dataset in tqdm(train_datasets):
-        mixed_train_instances.extend(dataset)
 
-    train_loader = DataLoader(
-        dataset=mixed_train_instances,
+    mixed_train_instances = []
+    if not args.eval_only:
+        for dataset in tqdm(train_datasets):
+            mixed_train_instances.extend(dataset)
+
+        train_loader = DataLoader(
+            dataset=mixed_train_instances,
+            collate_fn=model.collate_fn,
+            batch_size=BATCH_SIZE,
+            shuffle=True
+        )
+
+    mixed_dev_instances = []
+    for dataset in train_datasets:
+        if 'validation' in dataset.data:
+            dataset.split = "validation"
+            mixed_dev_instances.extend(dataset)
+            dataset.split = "train"
+
+
+
+    dev_loader = DataLoader(
+        dataset=mixed_dev_instances,
         collate_fn=model.collate_fn,
         batch_size=BATCH_SIZE,
-        shuffle=True
     )
 
-    print_average_task_mixing(train_loader)
+    if not args.eval_only:
+        print_average_task_mixing(train_loader)
 
-    logger = WandbLogger(project="biomuppet")
+    if not args.eval_only:
+        logger = WandbLogger(project="biomuppet")
+    else:
+        logger = None
     trainer = pl.Trainer(gpus=NUM_GPUS, strategy="ddp", precision=16,
                          callbacks=callbacks,
                          accumulate_grad_batches=GRADIENT_ACCUMULATION,
                          logger=logger, max_steps=MAX_STEPS, gradient_clip_val=5)
     model.num_training_steps = MAX_STEPS
 
-    print(f"Training on {len(train_datasets)} datasets with a total of {len(mixed_train_instances)} training instances...")
-    # Train the model
-    trainer.fit(
-        model=model, train_dataloaders=train_loader
-    )
+    if not args.eval_only:
+        print(f"Training on {len(train_datasets)} datasets with a total of {len(mixed_train_instances)} training instances...")
+        trainer.fit(
+            model=model, train_dataloaders=train_loader
+        )
+    if args.eval_only:
+        trainer.validate(
+            model=model, dataloaders=dev_loader
+        )
