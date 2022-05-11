19a91da58499dc6c0800c110d20dab272ebe6017
Natasha Seelam <nseelam1@gmail.com>
2022-05-11 18:31:50 -0400

    compares machamp data to blurb

--- BLURB/get_overlap.py
+++ BLURB/get_overlap.py
@@ -1,90 +1,31 @@
 """Get overlap between bigbio + BLURB
+
+I AM USING LINKBERT's BLURB DATASET AS OPPOSED TO PRE-PROCESSING FROM SCRATCH.
+
+Pubmed QA is really big, hence has several split configs.
+
+BLURB_datasets == dict of BLURB dataset name + reported BLURB size from microsoft table
+BLURB2BB == dict of BLURB dataset name to the bigbio dataset to load
+bb_configs == name of bigbio dataset and configs to load. NOTE PUBMED HAS MULTIPLE
+
+Compare OVERLAPS:
+    BLURB Train < -- > Test
+    BLURB Train < -- > Train
+
+
 """
 import bigbio
+import pandas as pd
 from pathlib import Path
 from datasets import load_dataset
 from datasets.dataset_dict import DatasetDict
 from typing import List, Dict, Set
 from nltk.tokenize import sent_tokenize
-
-# Train/Dev/Test Values from: https://microsoft.github.io/BLURB/tasks.html#dataset_chemprot
-BLURB_datasets = {
-    "BC5CDR-chem_hf": [5203, 5347, 5385],
-    "BC5CDR-disease_hf": [4182, 4244, 4424],
-    "NCBI-disease_hf": [5134, 787, 960],
-    "BC2GM_hf": [15197, 3061, 6325],
-    "JNLPBA_hf": [46750, 4551, 8662],
-    "ebmnlp_hf": [339167, 85321, 16364],
-    "chemprot_hf": [18035, 11268, 15745],
-    "DDI_hf": [25296, 2496, 5716],
-    "GAD_hf": [4261, 535, 534],
-    "BIOSSES_hf": [64, 16, 20],
-    "hoc_hf": [1295, 186, 371],
-    "HoC_hf": [1295, 186, 371],
-    "pubmedqa_hf": [450, 50, 500],
-    "bioasq_hf": [670, 75, 140],  # Task 7b
-}
-
-# BLURB names <-> BigBio
-BLURB2BB = {
-    "BC5CDR-chem_hf": [5203, 5347, 5385],
-    "BC5CDR-disease_hf": [4182, 4244, 4424],
-    "NCBI-disease_hf": [5134, 787, 960],
-    "BC2GM_hf": [15197, 3061, 6325],
-    "JNLPBA_hf": [46750, 4551, 8662],
-    "ebmnlp_hf": [339167, 85321, 16364],
-    "chemprot_hf": [18035, 11268, 15745],
-    "DDI_hf": [25296, 2496, 5716],
-    "GAD_hf": [4261, 535, 534],
-    "BIOSSES_hf": [64, 16, 20],
-    "hoc_hf": [1295, 186, 371],
-    "HoC_hf": [1295, 186, 371],
-    "pubmedqa_hf": [450, 50, 500],
-    "bioasq_hf": [670, 75, 140],  # Task 7b  
-}
-
-BLURB2BB = {
-    "BC5CDR-chem_hf": None,
-    "BC5CDR-disease_hf": "bc5cdr",
-    "NCBI-disease_hf": "ncbi_disease",
-    "BC2GM_hf": "gnormplus",
-    "JNLPBA_hf": None,
-    "ebmnlp_hf": "ebm_pico",
-    "chemprot_hf": "chemprot",
-    "DDI_hf": "ddi_corpus",
-    "GAD_hf": None,
-    "BIOSSES_hf": "biosses",
-    "hoc_hf": "hallmarks_of_cancer",
-    "HoC_hf": "hallmarks_of_cancer",
-    "pubmedqa_hf": "pubmed_qa",
-    "bioasq_hf": "bioasq_task_b",
-}
-
-#bb_configs = {
-#    "bc5cdr": ["bc5cdr_bigbio_kb"],
-#    "gnormplus": ["gnormplus_bigbio_kb"]
-#    "ncbi_disease": ["ncbi_disease_bigbio_kb"],
-#    "ebm_pico": ["ebm_pico_bigbio_kb"],
-#    "chemprot": ["chemprot_bigbio_kb"], # "chemprot_shared_task_eval_source", 
-#    "ddi_corpus": ["ddi_corpus_bigbio_kb"],
-#    "biosses": ["biosses_bigbio_pairs"],
-#    "hallmarks_of_cancer": ["hallmarks_of_cancer_bigbio_text"],
-#    "pubmed_qa": ["pubmed_qa_labeled_fold" + str(i) for i in range(1, 11)],
-#    "bioasq_task_b": ["bioasq_7b_bigbio_qa"],
-#}
+from constants import BLURB_datasets, text_pairs, ner_examples
 
 # ------------------------- #
 # BLURB dataset fxns
 # ------------------------- #
-
-def get_link_preprocessed_data(datapath: Path) -> DatasetDict:
-    """Gets pre-processed data from LINKBERT.
-    Uses HuggingFace's datasets to load.
-    """
-    files = [i.__str__().split("/")[-1] for i in datapath.glob("*.json")]
-    data_files = {item.split(".json")[0] : item for item in files}
-    return load_dataset(datapath.__str__(), data_files=data_files)
-
 def get_name(dpath: Path) -> str:
     """Return name from a datapath object"""
     return dpath.__str__().split("/")[-1]
@@ -93,56 +34,124 @@ def get_blurb_size(data: DatasetDict) -> List[int]:
     """Compute number of examples in each key"""
     return [data[key].num_rows for key in data.keys()]
 
-def get_blurb_sentences(data: DatasetDict) -> Dict[str, Set[str]]:
-    """Get sentences from each training split"""
-    return {key: set(data[key]["sentence"]) for key in data.keys()}
-
-# ------------------------- #
-# BigBio dataset fxns
-# ------------------------- #
-def get_bigbio_dataset(dpath: Path, name: str) -> List[DatasetDict]:
-    """Retrieve BigBio Dataset, given name"""
-    bbname = BLURB2BB[name] 
-
-    if bbname is None:
-        return None
-    else:
-        dsetname = bbname + ".py"
-        dpath = dpath / bbname / dsetname
-        return [load_dataset(dpath.__str__(), name=c) for c in bb_configs[bbname]]
-
-
-def split_sentences(data: List[List[Dict[str, str]]]) -> Set[str]:
-    """Given passages in a dataset, split into sentences
-
-    data: A given data split "passages" argument in the BB schema (ex: biodata["train"]["passages"])
+def get_linkbert_blurb_preprocessed_data(datapath: Path) -> DatasetDict:
+    """Gets pre-processed data from LINKBERT.
+    Uses HuggingFace's datasets to load.
     """
-    sents = set()
-    if len(data):
-        for psg in data:
-            if len(psg):
-                s = " ".join([" ".join(i["text"]) for i in psg])  # Smush all sentences together
-                sents = sents.union(set(sent_tokenize(s)))
-    return sents
-
-def get_bigbio_sentences():
-    """Return key: sentence"""
-    pass
+    files = [i.__str__().split("/")[-1] for i in datapath.glob("*.json")]
+    data_files = {item.split(".json")[0] : item for item in files}
+    return load_dataset(datapath.__str__(), data_files=data_files)
 
 
-if __name__ == "__main__":
-    curr_dirr = Path(__file__).__str__().split('/')[0]
-    data_dir = Path(curr_dirr) / "data"
+def get_blurb_sentences(data: DatasetDict, name: str) -> Dict[str, Set[str]]:
+    """Get sentences from each training split"""
+    if name in text_pairs:
+        output = {key: set(data[key]["sentence1"]) for key in data.keys()}
+        output = {key: output[key].union(set(data[key]["sentence2"])) for key in data.keys()}
+    
+    elif name in ner_examples:
+        output = {key: data[key]["tokens"] for key in data.keys()}
+        output = {key: set([j for k in output[key] for j in k]) for key in output.keys()}
+        
+    else:
+        output = {key: set(data[key]["sentence"]) for key in data.keys()}
+    return output
 
-    bigbio_path = Path("/home/natasha/Projects/hfbiomed/biomedical/biodatasets")
 
-    data_paths = list((data_dir / "seqcls").glob("*"))
-    data_paths += list((data_dir / "tokcls").glob("*"))
+def collect_blurb_data(data_paths: List[Path]) -> Dict[str, Set[str]]:
+    """Collects ALL BLURB train/val/test splits.
+    Creates dict holding each data split.
+
+    :param data_paths: Path locs for all BLURB datasets 
+    """
+    blurb_text_pairs = {"train": set(), "dev": set(), "test": set()}
+    blurb_ner = {"train": set(), "dev": set(), "test": set()}
+    blurb = {"train": set(), "dev": set(), "test": set()}
 
     for dpath in data_paths:
         name = get_name(dpath)
-        print("Dataset", name)
-        bdata = get_link_preprocessed_data(dpath)
-        assert(get_blurb_size(bdata) == BLURB_datasets[name], "Issue with " + name)
 
-        #bbiodata = get_bigbio_dataset(bigbio_path, name)
+        print("Dataset", name)
+        bdata = get_linkbert_blurb_preprocessed_data(dpath)
+        blurb_sentences = get_blurb_sentences(bdata, name)
+
+        if name in text_pairs:
+            for key in blurb_sentences.keys():
+                blurb_text_pairs[key] = blurb_text_pairs[key].union(blurb_sentences[key])
+        elif name in ner_examples:
+            for key in blurb_sentences.keys():
+                blurb_ner[key] = blurb_ner[key].union(blurb_sentences[key])
+        else:
+            for key in blurb_sentences.keys():
+                blurb[key] = blurb[key].union(blurb_sentences[key])
+
+    return blurb, blurb_ner, blurb_text_pairs
+
+# ------------------------- #
+# Machamp pre-processed data fxns
+# ------------------------- #
+
+def collect_machamp_data(datapath: Path) -> DatasetDict:
+    pass
+
+def get_machamp_data_per_task(datapath: Path, split: str) -> Dict[str, Set[str]]:
+    """Given a machamp task, construct a set of all sentences from all datasets in it
+    """
+    sents = set()
+    for fname in datapath.glob("*." + split):
+        x = pd.read_csv(fname, sep="\t") # Two columns, space separated
+        sents = sents.union(set(x.iloc[:, 0].tolist()))
+    
+    return {split: sents}
+
+
+# ------------------------- #
+# BigBio dataset fxns
+# DEPRECATED (2022.05.11) works directly on BB
+# ------------------------- #
+#def get_bigbio_dataset(dpath: Path, name: str) -> DatasetDict:
+#    """Retrieve BigBio Dataset, given name"""
+#    bbname = BLURB2BB[name] 
+#
+#    if bbname is None:
+#        return None
+#    else:
+#        dsetname = bbname + ".py"
+#        dpath = dpath / bbname / dsetname
+#        return load_dataset(dpath.__str__(), name=bb_configs[bbname])
+#
+#
+#def split_sentences(split: List[List[Dict[str, str]]]) -> Set[str]:
+#    """Given passages in a dataset, split into sentences
+#
+#    data: A given data split "passages" argument in the BB schema (ex: biodata["train"]["passages"])
+#    """
+#    sents = set()
+#    if len(split):
+#        for psg in split:
+#            if len(psg):
+#                s = " ".join([" ".join(i["text"]) for i in psg])  # Smush all sentences together
+#                sents = sents.union(set(sent_tokenize(s)))
+#    return sents
+
+#def get_bigbio_sentences(data: DatasetDict) -> Dict[str, Set[str]]:
+#    """Return split: set of sentences for bigbio data"""
+#    return {key: split_sentences(data[key]["passages"]) for key in data.keys()}
+    
+
+def compute_overlap(blurb: Set[str], bbio: Set[str]) -> Set[str]:
+    """Given 2 datasplits, compute set overlap between them"""
+    return blurb.intersection(bbio)
+
+if __name__ == "__main__":
+    data_dir = Path(Path(__file__).__str__().split('/')[0]) / "data"
+    machamp_dir = Path(__file__).parents[1] / "machamp/data/bigbio"
+
+    #data_paths = list((data_dir / "seqcls").glob("*"))
+    #data_paths += list((data_dir / "tokcls").glob("*"))
+
+    #blurb, blurb_ner, blurb_text_pairs = collect_blurb_data(data_paths)
+
+    # Get MACHAMP training data
+    tasks = list(machamp_dir.glob("*/"))
+
