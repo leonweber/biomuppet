62693286fe415bcb38f0bbd7a822440b4df57f62
Natasha Seelam <nseelam1@gmail.com>
2022-05-11 18:33:02 -0400

    remove irrelevant constants

--- BLURB/constants.py
+++ BLURB/constants.py
@@ -32,34 +32,3 @@ BLURB_datasets = {
     "pubmedqa_hf": [450, 50, 500],
     "bioasq_hf": [670, 75, 140],  # Task 7b
 }
-
-BLURB2BB = {
-    "BC5CDR-chem_hf": None,
-    "BC5CDR-disease_hf": "bc5cdr",
-    "NCBI-disease_hf": "ncbi_disease",
-    "BC2GM_hf": "gnormplus",
-    "JNLPBA_hf": None,
-    "ebmnlp_hf": "ebm_pico",
-    "chemprot_hf": "chemprot",
-    "DDI_hf": "ddi_corpus",
-    "GAD_hf": None,
-    "BIOSSES_hf": "biosses",
-    "hoc_hf": "hallmarks_of_cancer",
-    "HoC_hf": "hallmarks_of_cancer",
-    "pubmedqa_hf": "pubmed_qa",
-    "bioasq_hf": "bioasq_task_b",
-}
-
-# PUBMED QA is the only big one; process this separately
-bb_configs = {
-    "bc5cdr": "bc5cdr_bigbio_kb",
-    "gnormplus": "gnormplus_bigbio_kb",
-    "ncbi_disease": "ncbi_disease_bigbio_kb",
-    "ebm_pico": "ebm_pico_bigbio_kb",
-    "chemprot": "chemprot_bigbio_kb", # "chemprot_shared_task_eval_source", 
-    "ddi_corpus": "ddi_corpus_bigbio_kb",
-    "biosses": "biosses_bigbio_pairs",
-    "hallmarks_of_cancer": "hallmarks_of_cancer_bigbio_text",
-    "pubmed_qa": ["pubmed_qa_labeled_fold" + str(i) for i in range(1, 11)],
-    "bioasq_task_b": "bioasq_7b_bigbio_qa",
-}
