3c1b357114240534c7952c3e18375ed8e539e53b
Leon Weber <leonweber@posteo.de>
2022-05-28 10:58:31 +0200

    Fix overlap of names in config

--- biomuppet/generate_config.py
+++ biomuppet/generate_config.py
@@ -1,20 +1,23 @@
 import json
-from glob import glob
+from pathlib import Path
 
 from biomuppet.utils import DEBUG
 
 if __name__ == '__main__':
     full_config = {}
-    for config_file in glob("machamp/data/bigbio/*/config.json"):
+    for config_file in Path("machamp/data/bigbio").glob("*/config.json"):
         with open(config_file) as f:
             config = json.load(f)
-            for i, task in enumerate(config.values()):
-                if DEBUG and i > 3:
+            for i, (task_name, task_data) in enumerate(config.items()):
+                if DEBUG and i >= 3:
                     break
-                task["train_data_path"] = task["train_data_path"].replace("machamp/", "")
-                task["validation_data_path"] = task["validation_data_path"].replace("machamp/", "")
+                task_data = task_data.copy()
+                task_name = config_file.parent.name + "_" + "_".join(task_name.split("_")[:-1])
+                task_data["train_data_path"] = task_data["train_data_path"].replace("machamp/", "")
+                task_data["validation_data_path"] = task_data["validation_data_path"].replace("machamp/", "")
 
-            full_config.update(config)
+                full_config[task_name] = task_data
+
 
     if DEBUG:
         with open("machamp/configs/bigbio_debug.json", "w") as f:
